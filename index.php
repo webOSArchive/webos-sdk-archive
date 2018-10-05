29cdc3a827f73fec208e2c47e8acfcd35e35a00c
CodePoet80 <jonathan.m.wise@gmail.com>
2018-10-04 20:58:59 -0400

    try to split out header stuff

--- index.php
+++ index.php
@@ -1,35 +1,11 @@
 <!DOCTYPE html>
-<?php
-$palmdownloadserver="23.4.17.24"
-?>
 <html class="js" lang="en-US">
 <head>
-<link rel="stylesheet" type="text/css" href="assets/widget103.css" media="all">
+<?php
+    $pageTitle="SDK-PDK Download";
+    include("header.php");
+?>
 
-<meta name="generator" content="TYPO3 4.5 CMS">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<meta name="description" content="SDK-PDK Download">
-<meta name="keywords" content="Develop, Tools, SDK, PDK">
-<meta name="created" content="15.09.2011 20:35:27">
-<meta name="modified" content="15.09.2011 21:16:29">
-<meta name="robots" content="index, follow">
-<meta name="MSSmartTagsPreventParsing" content="true">
-<meta name="MSThemeCompatible" content="no">
-<meta name="imagetoolbar" content="false">
-
-<link rel="stylesheet" type="text/css" href="assets/stylesheet_5d370599a3.css" media="all">
-<!--[if lt IE 7]> <link rel="stylesheet" type="text/css" href="/content/typo3conf/ext/palm_template/css/ie6.1301600288.css" media="all"> <![endif]-->
-<!--[if IE 7]> <link rel="stylesheet" type="text/css" href="/content/typo3conf/ext/palm_template/css/ie7.1301600288.css" media="all"> <![endif]-->
-<!--[if IE 8]> <link rel="stylesheet" type="text/css" href="/content/typo3conf/ext/palm_template/css/ie8.1301600288.css" media="all"> <![endif]-->
-<!--[if IE 9 ]> <link rel="stylesheet" type="text/css" href="/content/typo3conf/ext/palm_template/css/ie9.1301600288.css" media="all"> <![endif]-->
-<!--[if gt IE 9 ]><link rel="stylesheet" type="text/css" href="/content/typo3conf/ext/palm_template/css/common.1301600288.css" media="all"><![endif]--><!--[if !IE]><!--> <link rel="stylesheet" type="text/css" href="assets/common.css" media="all"> <!--<![endif]-->
-<link rel="stylesheet" type="text/css" href="assets/additional.css" media="screen">
-
-<script src="assets/javascript_343c9f04e8.js" type="text/javascript"></script>
-
-<title> SDK-PDK Download - HP webOS Developer Center</title>			<link rel="shortcut icon" href="https://web.archive.org/web/20130130211140im_/https://developer.palm.com/content/typo3conf/ext/palm_template/icons/favicon.ico" type="application/x-empty; charset=binary">
-			<link rel="icon" href="https://web.archive.org/web/20130130211140im_/https://developer.palm.com/content/typo3conf/ext/palm_template/icons/favicon.ico" type="application/x-empty; charset=binary">
-<div style="visibility: hidden; height: 1px; width: 1px; position: absolute; z-index: 100000;" id="_atssh"><iframe id="_atssh752" title="AddThis utility frame" style="height: 1px; width: 1px; position: absolute; z-index: 100000; border: 0px none; left: 0px; top: 0px;" src="//web.archive.org/web/20130130212305/http://ct5.addthis.com/static/r07/sh111.html#"></iframe></div><script type="text/javascript" src="assets/core062.js"></script><script src="assets/ga.html" async="true"></script><script type="text/javascript" src="assets/core062.js"></script></head>
 <body class="PageArticle">
 
 <?php include 'menu.php'; ?>
@@ -623,17 +599,13 @@ $palmdownloadserver="23.4.17.24"
             target="_top">SDK-PDK Download</a>
 
             <ol class="level-1">
-              <li><a href="#c23272" target="_top">Installing the SDK and PDK on a Windows
-              PC</a></li>
+              <li><a href="#c23272" target="_top">Installing the SDK and PDK on a Windows PC</a></li>
 
-              <li><a href="#c23352" target="_top">Installing the SDK and PDK on a
-              Mac</a></li>
+              <li><a href="#c23352" target="_top">Installing the SDK and PDK on a Mac</a></li>
 
-              <li><a href="#c28522" target="_top">Installing the SDK on Ubuntu
-              Linux</a></li>
+              <li><a href="#c28522" target="_top">Installing the SDK on Ubuntu Linux</a></li>
 
-              <li class="last"><a href="#c39896" target="_top">Other Emulator
-              Images</a></li>
+              <li class="last"><a href="#c39896" target="_top">Other Emulator Images</a></li>
             </ol>
           </li>
         </ol>
