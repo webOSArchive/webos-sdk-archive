a835cbd0d90741124f9edcdebfb75cc61634f0d6
CodePoet80 <jonathan.m.wise@gmail.com>
2018-10-04 20:06:39 -0400

    split out footer

---
+++ footer.php
@@ -0,0 +1,51 @@
+
+<div id="footer">
+    <div class="hp-center">
+        <div class="line">
+        <div class="unit sizeFooter1">
+          <h4 class="aside-heading">Why webOS</h4>
+          <ul class="footer-list"><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/showcase/why_webos.html" target="_top">Business Case for webOS</a></li><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/showcase/developer_voices.html" target="_top">Success Stories</a></li><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/showcase/app_showcase.html" target="_top">App Showcase</a></li><li><a href="javascript:linkTo_UnCryptMailto('iwehpk6lzyWlwhi:yki');">Contact Us</a></li></ul>
+        </div>
+        <div class="unit sizeFooter2">
+          <h4 class="aside-heading">Getting Started</h4>
+          <ul class="footer-list highlight-footer-list"><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/index.php?option=com_user&amp;view=login&amp;signup" target="_blank">Join the HP webOS Developer Program</a></li><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/resources/develop/sdk_pdk_download.html" target="_top">Download the SDK/PDK</a></li><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/resources/develop/building_your_first_app.html" target="_top">Developing Your First App</a></li></ul>
+        </div>
+        <div class="unit sizeFooter3">
+          <h4 class="aside-heading">Videos</h4>
+          <ul class="footer-list"><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/resources/videos/videos/webos_connect.html" target="_top">webOS CONNECT Events</a></li><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/resources/videos/videos/webos_app_developer_conference.html" target="_top">MWC Developer Conference</a></li><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/resources/videos/videos/palm_developer_day_ny_2010.html" target="_top">NYC Developer Day</a></li><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/resources/videos/videos/developer_podcast_series.html" target="_top">Podcasts</a></li></ul>
+        </div>
+        <div class="unit sizeFooter4">
+          <h4 class="aside-heading">Support</h4>
+          <ul class="footer-list"><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/resources/footer/help.html" target="_top">Help</a></li><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/resources/develop/faq.html" target="_top">FAQs</a></li></ul>
+        </div>
+        <div class="unit lastUnit">
+          <div class="footer-resources">
+          <h4 class="aside-heading">Stay up to date</h4>
+          <ul class="footer-list rss-list"><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/resources/rss_feeds.html" target="_top">About RSS Feeds</a></li><li><a href="https://web.archive.org/web/20130130211140/http://developer.palm.com/blog" target="_blank">Developer Blog</a></li></ul>
+        </div>
+        </div>
+      </div>
+      <div class="imprint clearfix">
+        <div class="hp-copyright"><div id="c28323" class="csc-default"><p class="bodytext">© 2011 Hewlett-Packard Development Company, L.P.<br> The information contained herein is subject to change without notice. All screen images simulated. &nbsp;<a href="https://web.archive.org/web/20130130211140/http://www.palm.com/us/company/privacy.html" target="_blank">Privacy Statement</a>
+</p>
+<p class="bodytext">Supported&nbsp;browsers: Firefox 3.6+; Google Chrome 10+; Safari 5+; Internet Explorer 8+</p></div></div>
+        <ul class="inline-footer-list"><li><a href="https://web.archive.org/web/20130130211140/http://www.palm.com/" target="_top" title="Palm.com">Palm.com</a></li><li><a href="https://web.archive.org/web/20130130211140/http://www.palm.com/us/company/legal.html" target="_top" title="Legal Notices">Legal Notices</a></li><li><a href="https://web.archive.org/web/20130130211140/https://developer.palm.com/content/contact_us.html" target="_top" title="Contact Us">Contact Us</a></li></ul>
+      </div>
+    </div>
+</div>
+
+<!--[if lt IE 7 ]><script src="/content/typo3conf/ext/palm_template/js/libs/dd_belatedpng.js"></script><script> DD_belatedPNG.fix('img, .png_bg'); </script><![endif]-->
+<script src="assets/jquery-1.js" type="text/javascript"></script>
+<script src="assets/jquery_002.js" type="text/javascript"></script>
+<script src="assets/jquery.js" type="text/javascript"></script>
+<script src="assets/jquery_004.js" type="text/javascript"></script>
+<script src="assets/jquery_003.js" type="text/javascript"></script>
+<script src="assets/plugins.js" type="text/javascript"></script>
+<script src="assets/script.js" type="text/javascript"></script>
+<script src="assets/addthis_widget.js" type="text/javascript"></script>
+<script src="assets/javascript_7b66301c44.js" type="text/javascript"></script>
+
+
+
+
+<div id="fancybox-tmp"></div><div id="fancybox-loading"><div></div></div><div id="fancybox-overlay"></div><div id="fancybox-wrap"><div id="fancybox-outer"><div class="fancybox-bg" id="fancybox-bg-n"></div><div class="fancybox-bg" id="fancybox-bg-ne"></div><div class="fancybox-bg" id="fancybox-bg-e"></div><div class="fancybox-bg" id="fancybox-bg-se"></div><div class="fancybox-bg" id="fancybox-bg-s"></div><div class="fancybox-bg" id="fancybox-bg-sw"></div><div class="fancybox-bg" id="fancybox-bg-w"></div><div class="fancybox-bg" id="fancybox-bg-nw"></div><div id="fancybox-content"></div><a id="fancybox-close"></a><div id="fancybox-title"></div><a href="javascript:;" id="fancybox-left"><span class="fancy-ico" id="fancybox-left-ico"></span></a><a href="javascript:;" id="fancybox-right"><span class="fancy-ico" id="fancybox-right-ico"></span></a></div></div></body></html>
