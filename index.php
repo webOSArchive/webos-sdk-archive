e6f5d9ec1dbb6fe3b1d4fd8f9c6bac012bcb6283
codepoet80 <jonathan.m.wise@gmail.com>
2018-10-05 16:11:22 -0400

    add specific virtualbox versions

--- index.php
+++ index.php
@@ -171,7 +171,7 @@
           <p class="bodytext">The webOS emulator is built on VirtualBox&trade;, virtual
           machine software that you can download free from Oracle&reg;. VirtualBox is
           required before installing the HP webOS SDK. <b>Note:</b> The webOS emulator
-          now runs&nbsp;on Virtualbox 4.0 and&nbsp;4.1.</p>
+          now runs&nbsp;on Virtualbox 4.0.36 r71416 and&nbsp;4.1.0 r73009.</p>
 
           <p class="def-button"><a href=
           "https://www.virtualbox.org/wiki/Download_Old_Builds" target="_blank">Download
