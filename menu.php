87e6a066cb4796ccea6346d34df906d78b3b410f
codepoet80 <jonathan.m.wise@gmail.com>
2022-01-22 15:47:36 -0500

    respect protocol choice

--- menu.php
+++ menu.php
@@ -1,3 +1,7 @@
 <?php
+//Figure out what protocol the client wanted
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+echo file_get_contents("https://www.webosarchive.com/menu.php?content=sdk");
+else
 echo file_get_contents("http://www.webosarchive.com/menu.php?content=sdk");
 ?>
